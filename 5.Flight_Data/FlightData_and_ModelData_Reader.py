48ebcc18d982209aadafff79ec0bf5b849c47e0c
diegoomataix <diego.mataix@hotmail.com>
2021-12-02 16:03:44 +0100

    Interpolate_Data with moving average

--- 5.Flight_Data/FlightData_and_ModelData_Reader.py
+++ 5.Flight_Data/FlightData_and_ModelData_Reader.py
@@ -35,11 +35,11 @@ def flight_model_data():
     Reads the flight and model data and returns it and plots it separately
     """
     #### Read data (HTL) ####
-    HTL_fd = read_data('5.Flight_Data/1.Flight_Data_relevant/tc74s_interp.csv', [2,3,4,5,6,7])
+    HTL_fd = read_data('5.Flight_Data/1.Flight_Data_relevant/tc74s_interp.csv')
     # HTL_md = read_data('5.Flight_Data\2.Thermal_md\HTL_node_temps.csv')
     #### Read data (Other temps) ####
     Temps_fd = read_data(
-        '5.Flight_Data/1.Flight_Data_relevant/pt1000s_interp.csv.', [2, 3, 6, 9, 12, 15, 18, 2])
+        '5.Flight_Data/1.Flight_Data_relevant/pt1000s_interp.csv')
     # Temps_md = read_data('5.Flight_Data\2.Thermal_md\Temps_node_temps.csv')
     #### Plot data (HTL) ####
     plot_data(HTL_fd, len(HTL_fd.columns), 'HTL Flight Data Temperatures',
@@ -55,14 +55,14 @@ def flight_model_data():
     return HTL_fd, Temps_fd # , HTL_md, Temps_md
     
 #%% Read data ##################################################################
-def read_data(filename, ncols):
+def read_data(filename):
     """
     Reads data from a .csv file and returns a numpy array
     Inputs: 
         - filename - the name of the file to be read
         - ncols - the number of columns in the file as a list []
     """
-    data = pd.read_csv(filename, usecols=ncols)
+    data = pd.read_csv(filename)
     return data
 
 #%% Plot and compare data ######################################################
--- 5.Flight_Data/Interpolate_Data.py
+++ 5.Flight_Data/Interpolate_Data.py
@@ -4,7 +4,7 @@ This program reads data from .csv files, interpolates it to get a smooth curve, 
 #%% Import libraries ###########################################################
 import numpy as np
 import pandas as pd
-from scipy.interpolate import make_interp_spline, BSpline
+from scipy.interpolate import make_interp_spline, BSpline, splev, splrep
 import csv
 
 #%% Main #######################################################################
@@ -15,14 +15,15 @@ def main():
     # Read data
     HTL_fd = read_data('5.Flight_Data/1.Flight_Data_relevant/tc74s.csv', [2,3,4,5,6,7])
     Temps_fd = read_data('5.Flight_Data/1.Flight_Data_relevant/pt1000s.csv', [2,3,6,9,12,15,18,2])
-    #print(HTL_fd)
-    # Interpolate data
-    HTL_fd_interp = interpolate_data(HTL_fd)
-    Temps_fd_interp = interpolate_data(Temps_fd)
-    # Save data
+
+    ## Interpolate data
+    HTL_fd_interp = interpolate_data(HTL_fd,500)
+    #print(HTL_fd_interp)
+    Temps_fd_interp = interpolate_data(Temps_fd,500)
+    ## Save data
     save_data(HTL_fd_interp, '5.Flight_Data/1.Flight_Data_relevant/tc74s_interp.csv')
     save_data(Temps_fd_interp, '5.Flight_Data/1.Flight_Data_relevant/pt1000s_interp.csv')
-
+#
 #%% Read data ##################################################################
 def read_data(filename, ncols):
     """
@@ -32,23 +33,39 @@ def read_data(filename, ncols):
         - ncols - the number of columns in the file as a list []
     """
     data = pd.read_csv(filename, usecols=ncols)
-    return data.values
+    return data#.values
 
 #%% Interpolate data ##########################################################
-def interpolate_data(data): #NEEDS FIXING
+#def interpolate_data(data): #NEEDS FIXING
+#    """
+#    Interpolates data to get a smooth curve using scipy.interpolate.BSpline
+#    Inputs: 
+#       - data - the data to be interpolated, the first column is the time, the rest are the values. Interpolate for each column separately
+#       Ignore the time column
+#    """
+#    #data_interp = np.zeros((data.shape[0], data.shape[1]))
+#    #data_interp[:, 0] = data[:, 0]
+#    #for i in range(1, data.shape[1]):
+#    #    tck = BSpline(data[:, 0], data[:, i], k=3)
+#    #    data_interp[:, i] = splev(data_interp[:, 0], tck, der=0)
+#    #return data_interp
+#    data_interp = np.zeros((data.shape[0], data.shape[1]))
+#    data_interp[:, 0] = data[:, 0]
+#    for i in range(1, data.shape[1]):
+#        data_interp[:, i] = np.interp(
+#            data_interp[:, 0], data[:, 0], data[:, i])
+#    return data_interp
+
+def interpolate_data(data,window=100):
+    """ 
+    Use pandas rolling method to take the moving average of specific columns of a pandas dataset. 
     """
-    Interpolates data to get a smooth curve using scipy.interpolate.BSpline
-    Inputs: 
-        - data - the data to be interpolated, the first column is the time, the rest are the values. Interpolate for each column separately
-        Ignore the time column
-    """
-    # Interpolate using scipy.interpolate.BSpline for each column
+    # Create a new dataframe with the moving average of the columns
+    data_interp = pd.DataFrame(index=data.index)
+    data_interp[data.columns[0]] = data[data.columns[0]]
     for i in range(1, data.shape[1]):
-        x = np.array(range(len(data)))
-        spl = make_interp_spline(x, data, k=3)
-        xnew = np.linspace(0, len(data)-1, 300)
-        ynew = spl(xnew)
-    return xnew, ynew
+        data_interp[data.columns[i]] = data[data.columns[i]].rolling(window=window).mean()
+    return data_interp.values
 
 #%% Save data ##################################################################
 def save_data(data, filename):
